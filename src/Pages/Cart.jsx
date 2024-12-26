91548197e851b614f2c6c44b7535badddf026a3e
Shriganesh Bhandari <shriganesh.bhandari693@gmail.com>
2024-12-26 17:46:23 +0530

    Added Display product page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -10,41 +10,43 @@ const Cart = () => {
   const shippingFees = 10;
 
   useEffect(() => {
-    const amount = cartProducts.reduce(
-      (acc, product) => acc + product.price,
-      0
-    ) || 0;
+    const amount =
+      cartProducts.reduce((acc, product) => acc + product.price, 0) || 0;
     setTotalAmount(amount);
   }, [cartProducts]);
 
   return (
     <div className="border-t-2 flex flex-col">
       <div className="mb-10">
-      <div className="py-4 flex">
-        <Title titleOne={"Your"} titleTwo={"Cart"} />
-      </div>
+        <div className="py-4 flex">
+          <Title titleOne={"Your"} titleTwo={"Cart"} />
+        </div>
 
-      {cartProducts.map((product) => {
-        return (
-          <div key={product._id}>
-            <div className="flex border-t border-b w-full p-2 items-center justify-between">
-              <div className="flex gap-6 items-center">
-                <img src={product.image[0]} alt="" className="w-[100px]" />
-                <div className="flex flex-col gap-4">
-                  <p>{product.name}</p>
-                  <div className="flex gap-4 items-center">
-                    <p className="font-medium">${product.price}</p>
-                    <p className="px-2 py-1 border bg-gray-100">M</p>
+        {cartProducts.map((product) => {
+          return (
+            <div key={product._id}>
+              <div className="flex border-t border-b w-full p-2 items-center justify-between">
+                <div className="flex gap-6 items-center">
+                  <img src={product.image[0]} alt="" className="w-[100px]" />
+                  <div className="flex flex-col gap-4">
+                    <p>{product.name}</p>
+                    <div className="flex gap-4 items-center">
+                      <p className="font-medium">${product.price}</p>
+                      <p className="px-2 py-1 border bg-gray-100">M</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                <img src={assets.bin_icon} alt="" className="cursor-pointer w-7" />
+                <div>
+                  <img
+                    src={assets.bin_icon}
+                    alt=""
+                    className="cursor-pointer w-7"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       </div>
 
       <div className="mb-20">
@@ -65,13 +67,11 @@ const Cart = () => {
             <p className="font-semibold">$ {totalAmount + shippingFees}.00</p>
           </div>
         </div>
-
         <button className="uppercase px-7 py-2 bg-black text-white mt-10 w-fit">
           Proceed to checkout
         </button>
       </div>
       <Footer />
-
     </div>
   );
 };
